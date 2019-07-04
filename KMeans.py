67c2adc3aafa135d9fc49f25a5ed0490b2d91ba6
jbrammers <james.bramley@outlook.com>
2019-07-04 20:25:33 +0100

    Classifiers used to predict the dataset of 294427 all uploaded. More tuning required but current best are knn (n=1) and random forrest (estimators=100) both at just over 90% accuracy.

---
+++ KMeans.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split as tts
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+fileName = '294427'
+df = pd.read_csv('./CSVs/' + fileName + 'Drop.csv', index_col=0).transpose().to_numpy()
+lf = pd.read_csv('./CSVs/' + fileName + 'Labels.csv', index_col=0).transpose().to_numpy()
+
+scores = []
+X_train, X_test, y_train, y_test = tts(df, np.ravel(lf), test_size=0.2)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Below for loop used to determine the best number of neighbours
+
+# rangeK = range(1, 25)
+# for k in rangeK:
+#     model = knn(n_neighbors=k)
+#     model.fit(X_train, y_train)
+#     pred = model.predict(X_test)
+#     scores.append(metrics.accuracy_score(pred, y_test))
+#
+# plt.plot(rangeK, scores)
+# plt.show()
+
+model = KMeans(n_clusters=3)
+model.fit(X_train, y_train)
+pred = model.predict(X_test)
+print(metrics.accuracy_score(pred, y_test))
---
+++ NB.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split as tts
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+
+fileName = '294427'
+df = pd.read_csv('./CSVs/' + fileName + 'Drop.csv', index_col=0).transpose().to_numpy()
+lf = pd.read_csv('./CSVs/' + fileName + 'Labels.csv', index_col=0).transpose().to_numpy()
+
+scores = []
+X_train, X_test, y_train, y_test = tts(df, np.ravel(lf), test_size=0.2)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Below for loop used to determine the best number of estimators
+# rangeN = range(10, 500, 10)
+# for n in rangeN:
+#     avg = []
+#     for k in range(1,5):
+#         model = RandomForestClassifier(n_estimators=n, n_jobs=-1)
+#         model.fit(X_train, y_train)
+#         pred = model.predict(X_test)
+#         avg.append(metrics.accuracy_score(y_test, pred))
+#     scores.append(np.average(avg))
+# plt.plot(rangeN, scores)
+# plt.show()
+
+
+model = GaussianNB()
+model.fit(X_train, y_train)
+pred = model.predict(X_test)
+print(metrics.accuracy_score(y_test, pred))
