3994737eebcffbb53870bf0ba0ccab45fa9efe5f
James Bramley <jxb1775@cs.bham.ac.uk>
2019-07-08 19:25:28 +0100

    Adding functions to each python script so they can be called from elsewhere.

--- RF.py
+++
@@ -1,9 +0,0 @@
-from sklearn.ensemble import RandomForestRegressor as rf
-import pandas as pd
-
-fileName = '294427Drop'
-signal = pd.read_csv('./CSVs/' + fileName + '.csv')
-#
-# print(signal.head())
-# print(signal.describe())
-# print(signal.shape)
